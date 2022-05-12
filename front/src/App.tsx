05fa96eb1e7c1a25aeb7245d3179d69e18c68425
Hato <12556403+hatoPoPoPoPo@users.noreply.github.com>
2022-05-12 15:19:04 +0900

    router add

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function Home() {
+  return (
+      <div>
+        home pageです
+      </div>
+  );
+}
+
+function Profile() {
+  return (
+      <div>
+        profile pageです
+      </div>
+  );
+}
+
+function About() {
+  return (
+      <div>
+        about pageです
+      </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Hello world!</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
   );
 }
 
